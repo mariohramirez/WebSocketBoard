96a216cefec4c4af325ac8d3764edd3afe6787b8
Mario Ramírez <marioh.ramirez@udea.edu.co>
2023-10-09 17:45:55 -0500

    Agregado canvas

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import CanvasDrawing from '@/Components/Canvas';
+
+const Index=() => {
+  return (
+    <div>
+      <h1 className=' text-cyan-400'>Tablero</h1>
+      <CanvasDrawing />
+    </div>
+  );
+};
+
+export default Index;
